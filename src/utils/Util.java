9fbff00340d00d2cece32cb4d334e19942395ed7
Sofia <sofialpizard@gmail.com>
2018-07-31 19:05:01 -0600

    add method to get driver

--- src/utils/Util.java
+++ src/utils/Util.java
@@ -1,10 +1,19 @@
 package utils;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Util {
 	
 	public static String getMacChromeDriver() {
 		String url = "/Users/sofia/Documents/workspace/WebDriverTest/chromedriver";
 		return url;
+	}
+	
+	public static WebDriver getDriver() {
+		System.setProperty("webdriver.chrome.driver", Util.getMacChromeDriver());
+		WebDriver driver = new ChromeDriver();
+		return driver;
 	}
 
 }
