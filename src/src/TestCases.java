a3a27f7566b2211b15edecaf295fcb115ad20ac6
Sofia <sofialpizard@gmail.com>
2018-07-31 18:57:05 -0600

    Util class to handle the web driver exec url

--- src/src/TestCases.java
+++ src/src/TestCases.java
@@ -1,9 +1,18 @@
 package src;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import utils.Util;
+
 public class TestCases {
+	
+	static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    static PrintStream out = System.out;    // variables objetos in y out para
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", Util.getMacChromeDriver());
 
 	}
 
---
+++ src/utils/Util.java
@@ -0,0 +1,10 @@
+package utils;
+
+public class Util {
+	
+	public static String getMacChromeDriver() {
+		String url = "/Users/sofia/Dropbox/Trabajos\\ U/Calidad\\ y\\ verificacion/Investigacion2/SeleniumTestCases/chromedriver ";
+		return url;
+	}
+
+}
