b51765456066cb9b4d80065ac003cd5c026353e7
DeusAnimaX <ernest.27@hotmail.com>
2018-08-03 02:02:50 -0600

    TC1 y TC2 v1.0

--- src/src/TestCases.java
+++ src/src/TestCases.java
@@ -5,11 +5,14 @@ import static org.junit.Assert.assertEquals;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
@@ -20,6 +23,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import utils.Util;
 import Data.ReadData;
+import junit.framework.Assert;
 
 public class TestCases {
 	
@@ -28,8 +32,114 @@ public class TestCases {
 
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-		testCase5(Util.getDriver());
+		testCase2(Util.getDriver());
 	}
+	
+	public static void testCase1(WebDriver driver) throws Exception {
+		// Inicializa el web driver para peticiones sincronicas
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		
+		// Carga la pagina web
+		driver.get("http://demo.nopcommerce.com");
+		
+		// Busca las opciones del menu principal
+		String xpath = "//ul[@class='top-menu']/li/a[@href]";
+		List<WebElement> menuOptions = driver.findElements(By.xpath(xpath));
+		List<String> optionKeys = new ArrayList<String>();
+		
+		// Guarda el identificador de cada opcion de menu para consultarlo despues de cada redireccionamiento
+		for (WebElement element : menuOptions) {
+			String redirectPage = element.getAttribute("href");
+			redirectPage = redirectPage.substring(redirectPage.lastIndexOf('/'));
+			optionKeys.add(redirectPage);
+		}
+		
+		// Busca la opcion de menu en la pagina, la ejecuta y espera 3 segundos para el siguiente
+		for (String key : optionKeys) {
+			String optionXPath = "//ul[@class='top-menu']/li/a[@href='" + key + "']";
+			WebElement btn = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(optionXPath)));
+			btn.click();
+		}
+		
+		// Busca el logo principal del sitio y ejecuta el 'click' que redirecciona a la pagina principal
+		xpath = "//div[@class='header-logo']/a[@href='/']";
+		WebElement pageLogo = driver.findElement(By.xpath(xpath));
+		pageLogo.click();
+		
+		// Ejecuta codigo JavaScript para mostrar un popup
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("alert('Test finalized successfully!')");
+    }
+
+	public static void testCase2(WebDriver driver) throws Exception {
+		// Inicializa el web driver para peticiones sincronicas
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		
+        // Carga la pagina web
+        driver.get("http://demo.nopcommerce.com/wishlist");
+
+        String xpath = "//div[@class='no-data']";
+        
+        // Obtiene el mensaje 'The wish list is empty';
+        String message = driver.findElement(By.xpath(xpath)).getText();
+
+        Assert.assertTrue(message.equals("The wishlist is empty!"));
+        
+        String searchBarPath = "//input[@id='small-searchterms']";
+        //ingresa el texto de busqueda y ejecuta un enter
+        
+        driver.findElement(By.xpath(searchBarPath)).sendKeys("Fahrenheit 451"+Keys.ENTER);
+        String btnWishPath = "//input[@class='button-2 add-to-wishlist-button']";
+        
+        //agrega el libro al wish list
+        WebElement btn = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(btnWishPath)));
+        btn.click();
+
+        String btnWishList = "//a[@class='ico-wishlist']";
+        
+        //regresa a la pagina de whishlist
+        btn = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(btnWishList)));
+        btn.click();
+
+        //verifica si agrego el libro Fahrenheit 451
+        String wishListItem = "//a[@class='product-name']";
+        String wishItemName = driver.findElement(By.xpath(wishListItem)).getText();
+        Assert.assertTrue(wishItemName.contains("Fahrenheit 451"));
+        
+        // Redirige al cart
+        String shoppingCartButton = "//li[@id=\"topcartlink\"]/a[@href=\"/cart\"]";
+        btn = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(shoppingCartButton)));
+        btn.click();
+        
+        String cartEmptyMessage = "//div[@class=\"no-data\"]";
+        message = driver.findElement(By.xpath(cartEmptyMessage)).getText();
+        
+        Assert.assertTrue(message.equals("Your Shopping Cart is empty!"));
+        
+        String wishListButton = "//div[@class=\"header-links\"]/ul/li/a[@href=\"/wishlist\"]";
+        btn = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(wishListButton)));
+        btn.click();
+        
+        String addToCartCheck = "//table[@class=\"cart\"]/tbody/tr/td[@class=\"add-to-cart\"]/input";
+        btn = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(addToCartCheck)));
+        btn.click();
+        
+        String addToCartButton = "//div[@class=\"buttons\"]/input[@name=\"addtocartbutton\"]";
+        btn = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(addToCartButton)));
+        btn.click();
+        
+        String cartListItem = "//a[@class='product-name']";
+        String cartItemName = driver.findElement(By.xpath(cartListItem)).getText();
+        Assert.assertTrue(cartItemName.contains("Fahrenheit 451"));
+        
+        String continueButton = "//div[@class=\"common-buttons\"]/input[@name=\"continueshopping\"]";
+        btn = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(continueButton)));
+        btn.click();
+        
+        // Ejecuta codigo JavaScript para mostrar un popup
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("alert('Test 2 finalized successfully!')");
+    }
 	
 	public static void testCase5(WebDriver driver) throws Exception {
     	driver.get("http://demo.nopcommerce.com/wishlist");
